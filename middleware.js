c6c3c83111e4b6a7b92287f8cdd3e17675cb0cfc
Pranchal Agrahari <pranchalagrahari@gmail.com>
2021-03-13 11:18:08 +0530

    added redirect url

--- middleware.js
+++ middleware.js
@@ -1,8 +1,10 @@
 module.exports.isloggedin=(req,res,next)=>{
     if(!req.isAuthenticated())
   {
+    //console.log(req.originalUrl);
+    req.flash('error', 'You must be signed in first!');
     req.session.returnTo = req.originalUrl;
-    return res.redirect("/");
+    return res.redirect("/login");
   }
   next();
 }
